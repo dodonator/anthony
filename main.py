14bb0c3f723b8a80d6d85f446157db2ce74a3b3e
dodo <dodo@chaosdorf.de>
2022-01-13 18:44:56 +0100

    implemented a prototype version

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-import pandoc
-from pandoc.types import Header
-from pathlib import Path
+import cmd
+import datetime
+from collections import namedtuple
+from typing import List
 
-filename = "bullet_journal.md"
-filepath = Path(filename)
+Task = namedtuple("Task", ("id", "title", "description", "status", "creation"))
 
-with filepath.open("r") as file:
-    text = file.read()
 
-doc = pandoc.read(text)
+class BulletJournalShell(cmd.Cmd):
+    intro = "Welcome to you bullet journal. Type help or ? to list commands.\n"
+    prompt = "[bullet-journal] "
+    journal: List[Task] = list()
+    task_id = 0
 
-for element in doc[1]:
-    if isinstance(element, Header):
-        header_title = element[2]
-        print(f"{header_title!r}")
+    def do_add(self, line):
+        """
+        Usage:
+        add [title], [description], [status], [creation]
+        """
+        elements = line.strip().split(",")
+        elements = list(map(str.strip, elements))
+        elements += [None] * (4 - len(elements))
+        title = elements[0]
+        description = elements[1] if elements[1] is not None else ""
+        status = int(elements[2]) if elements[2] is not None else 0
+        creation_date = (
+            datetime.date.fromisoformat(elements[3])
+            if elements[3] is not None
+            else datetime.date.today()
+        )
+        task = Task(self.task_id, title, description, status, creation_date)
+        print(f"created {task}")
+        self.journal.append(task)
+        self.task_id += 1
+
+    def do_list(self, line):
+        """lists all tasks"""
+        for task in self.journal:
+            print(f"{task.id} | {task.title} | {task.creation}")
+
+    def postcmd(self, stop, line):
+        # prints a new line after each command
+        print()
+        return cmd.Cmd.postcmd(self, stop, line)
+
+
+if __name__ == "__main__":
+    BulletJournalShell().cmdloop()
