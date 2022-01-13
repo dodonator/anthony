4cb3ae920dafb52cbeceaedab8892f57e6375c01
dodo <dodo@chaosdorf.de>
2022-01-13 21:03:54 +0100

    implemented execution date instead of creation date

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from yaml import CDumper as Dumper
 from yaml import CLoader as Loader
 from yaml import dump, load
 
-Task = namedtuple("Task", ("id", "title", "description", "status", "creation"))
+Task = namedtuple("Task", ("id", "title", "description", "status", "execution", "creation"))
 
 
 class BulletJournalShell(cmd.Cmd):
@@ -20,7 +20,7 @@ class BulletJournalShell(cmd.Cmd):
     def do_add(self, line):
         """
         Usage:
-        add [title], [description], [status], [creation]
+        add [title], [description], [status], [execution]
         """
         elements = line.strip().split(",")
         elements = list(map(str.strip, elements))
@@ -28,20 +28,35 @@ class BulletJournalShell(cmd.Cmd):
         title = elements[0]
         description = elements[1] if elements[1] is not None else ""
         status = int(elements[2]) if elements[2] is not None else 0
-        creation_date = (
-            datetime.date.fromisoformat(elements[3])
-            if elements[3] is not None
-            else datetime.date.today()
-        )
-        task = Task(self.task_id, title, description, status, creation_date)
+        execution_date = datetime.date.fromisoformat(elements[3]) if elements[3] is not None else datetime.date.today()
+        creation_date = datetime.date.today()
+
+        task = Task(self.task_id, title, description, status, execution_date, creation_date)
         print(f"created {task}")
         self.journal.append(task)
         save_task(task, self.path)
         self.task_id += 1
 
     def do_list(self, line):
-        """lists all tasks"""
-        for task in self.journal:
+        """
+        Usage:
+        list all | today | open | closed
+        """
+        today = datetime.date.today()
+        match line.strip():
+            case "today":
+                task_list = list(filter(lambda t: t.execution == today, self.journal))
+
+            case "open":
+                task_list = list(filter(lambda t: t.status == 0, self.journal))
+
+            case "closed":
+                task_list = list(filter(lambda t: t.status == 1, self.journal))
+
+            case "all" | "":
+                task_list = self.journal
+
+        for task in task_list:
             print(f"{task.id} | {task.title} | {task.creation}")
 
     def do_close(self, line):
@@ -55,7 +70,7 @@ class BulletJournalShell(cmd.Cmd):
             self.task_id = 0
         else:
             self.journal = journal
-            self.task_id = journal.pop().id + 1
+            self.task_id = journal[-1].id + 1
 
         return super().preloop()
 
@@ -72,7 +87,7 @@ def save_tasks(task_list: List[Task], path: Path):
 
 def save_task(task: Task, path: Path):
     with path.open("a") as file:
-        dump([task], file, Dumper=Dumper)
+        dump([task._asdict()], file, Dumper=Dumper)
 
 
 def load_tasks(path: Path):
