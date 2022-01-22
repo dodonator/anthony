5ccd7219bdd64da63d2205d4393ee307a4165826
dodo <dodo@chaosdorf.de>
2022-01-22 16:05:15 +0100

    implemented preloop

--- anthonyShell.py
+++ anthonyShell.py
@@ -1,5 +1,9 @@
 import cmd
 import sys
+from pathlib import Path
+
+from IO import init_dir, initialize_page
+from Page import Page
 
 
 class AnthonyShell(cmd.Cmd):
@@ -8,12 +12,28 @@ Welcome to anthony your bullet journal.
 Type help or ? to list commands.
 """
     prompt = "[anthony] "
+    source_path = Path("./source/")
+    current_page: Page
+    current_path: Path
 
     # ToDo: implement commands
 
     def do_exit(self, line):
         """Exits the programm."""
         sys.exit()
+
+    def do_info(self, line):
+        """Prints info concerning the current working directory."""
+        print(f"source path: {self.source_path}")
+        print(f"current page: {self.current_page}")
+        print(f"current page (path): {self.current_path}")
+
+    def preloop(self) -> None:
+        init_dir(self.source_path)
+        page, path = initialize_page(self.source_path)
+        self.current_page = page
+        self.current_path = path
+        return super().preloop()
 
 
 if __name__ == "__main__":
