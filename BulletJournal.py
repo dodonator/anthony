54a2e3244c16c83a27d4e1a92c5fa3e056a2ce7f
dodo <dodo@chaosdorf.de>
2022-01-22 15:42:43 +0100

    created initialize_page

--- BulletJournal.py
+++ BulletJournal.py
@@ -1,6 +1,5 @@
 import datetime
 from pathlib import Path
-from pprint import pprint
 
 from IO import load_last_page, save_page
 from Page import Page
@@ -12,29 +11,27 @@ def active_tasks(page: Page) -> list[Page]:
     return active_tasks
 
 
-today = datetime.date.today()
-source_path = Path(".")
+def initialize_page(source_path: Path):
+    today = datetime.date.today()
 
-last_page = load_last_page(source_path)
+    last_page = load_last_page(source_path)
 
-if last_page.date != today:
-    # load active tasks
-    old_tasks = active_tasks(last_page)
+    if last_page.date != today:
+        # load active tasks
+        old_tasks = active_tasks(last_page)
 
-    # create new page
-    page = Page(today)
+        # create new page
+        page = Page(today)
 
-    # add old tasks to page
-    for task in old_tasks:
-        page.add(task)
+        # add old tasks to page
+        for task in old_tasks:
+            page.add(task)
 
-    # create path for page
-    page_path = source_path / Path(f"{page.date.isoformat()}.yaml")
-    page_path.touch()
+        # create path for page
+        page_path = source_path / Path(f"{page.date.isoformat()}.yaml")
+        page_path.touch()
 
-    # save page to path
-    save_page(page_path, page)
-else:
-    page = last_page
-
-pprint(page.to_dict())
+        # save page to path
+        save_page(page_path, page)
+    else:
+        page = last_page
