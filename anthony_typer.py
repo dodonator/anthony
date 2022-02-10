c4acfda27dd16df50c30e0042c1df144e8500409
dodo <dodo@chaosdorf.de>
2022-02-10 19:25:34 +0100

    added command templates

--- anthony_typer.py
+++ anthony_typer.py
@@ -2,5 +2,21 @@ import typer
 
 app = typer.Typer()
 
+
+@app.command()
+def add(type: str, title: str):
+    pass
+
+
+@app.command()
+def list(type: str = ""):
+    pass
+
+
+@app.command()
+def delete(title: str):
+    pass
+
+
 if __name__ == "__main__":
     app()
