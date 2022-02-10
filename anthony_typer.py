f7fe10830aeb3dedeabc04cbafa40f3cf1a6d5cf
dodo <dodo@chaosdorf.de>
2022-02-10 20:23:25 +0100

    implemented list and add note

--- anthony_typer.py
+++ anthony_typer.py
@@ -1,16 +1,53 @@
+from pathlib import Path
+
 import typer
+
+from IO import init_dir, initialize_page, save_page
+from Note import Note
+
+source_path = Path("./source/")
 
 app = typer.Typer()
 
 
 @app.command()
-def add(type: str, title: str):
-    pass
+def add(item_type: str):
+    if item_type == "appointment":
+        pass
+
+    elif item_type == "note":
+        title = typer.prompt("title")
+        description = typer.prompt("description")
+        note = Note(title, description)
+        page.add(note)
+        typer.echo(f"added {note}")
+
+    elif item_type == "task":
+        pass
+
+    else:
+        pass
+
+    save_page(path, page)
 
 
 @app.command()
-def list(type: str = ""):
-    pass
+def list(item_type: str = ""):
+
+    if item_type == "appointment":
+        items = page.appointments()
+
+    elif item_type == "note":
+        items = page.notes()
+
+    elif item_type == "task":
+        items = page.tasks()
+
+    else:
+        items = list(page)
+
+    for item in items:
+        typer.echo(item)
 
 
 @app.command()
@@ -19,4 +56,7 @@ def delete(title: str):
 
 
 if __name__ == "__main__":
+    init_dir(source_path)
+    page, path = initialize_page(source_path)
     app()
+    save_page(path, page)
