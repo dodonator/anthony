80340d8546e3223e3474aaa40d993fe3c45ed266
dodo <dodo@chaosdorf.de>
2022-03-03 20:15:36 +0100

    output methods for item instances

--- model.py
+++ model.py
@@ -18,6 +18,35 @@ class Item(BaseModel):
             v = uuid4().hex
         return v
 
+    def __str__(self) -> str:
+        """Returns the item title as string representation.
+
+        Returns:
+            str: title
+        """
+        return self.title
+
+    def __repr__(self) -> str:
+        """Returns a summary of the item as str.
+
+        Returns:
+            str: item attributes
+        """
+        attrs = self.__dict__.items()
+        kv_pairs = [f"{key}={value!r}" for key, value in attrs if key != "id"]
+        return f"{self.__class__.__name__}({', '.join(kv_pairs)})"
+
+    def __rich__(self) -> dict:
+        """Returns the dict for using rich.
+
+        Returns:
+            _type_: item dict
+        """
+        return self.__dict__
+
+    def __hash__(self) -> str:
+        return self.id
+
 
 class Appointment(Item):
     start: datetime.datetime
