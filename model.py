714dbb547b2f97146200692cce02caaa14aaf419
dodo <dodo@chaosdorf.de>
2022-03-03 18:49:59 +0100

    added validator to create uuid

--- model.py
+++ model.py
@@ -1,14 +1,23 @@
 import datetime
 from typing import Optional
+from uuid import UUID, uuid4
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Appointment(BaseModel):
-    id: Optional[str]
+    id: Optional[str] = None
     title: str
     content: str
     start: datetime.datetime
+
+    @validator("id")
+    def id_must_be_valid_uuid(cls, v):
+        if v is not None:
+            v = UUID(v).hex
+        else:
+            v = uuid4().hex
+        return v
 
 
 class Note(BaseModel):
