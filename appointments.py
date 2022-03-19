e158f513eb3f8083b4ef8d8d67e1c6087184307f
dodo <dodo@chaosdorf.de>
2022-03-19 20:08:39 +0100

    implemented add appointment

--- appointments.py
+++ appointments.py
@@ -2,17 +2,23 @@ from datetime import datetime
 
 import typer
 
-from model import Appointment
+from IO import date_to_path, load_page, save_page
+from model import Appointment, Page
 
 app = typer.Typer()
-
-if __name__ == "__main__":
-    app()
 
 
 @app.command()
 def add():
-    pass
+    appointment: Appointment = input_appointment()
+    date = appointment.start.date()
+    path = date_to_path(date)
+    page: Page | None = load_page(path)
+    if page is None:
+        page = Page(date)
+    page.add(appointment)
+    save_page(page)
+    typer.echo(f"saved appointment {appointment} at {path}")
 
 
 @app.command()
@@ -37,3 +43,7 @@ def input_appointment() -> Appointment:
     appointment_data["start"] = appointment_start
     appointment = Appointment(**appointment_data)
     return appointment
+
+
+if __name__ == "__main__":
+    app()
